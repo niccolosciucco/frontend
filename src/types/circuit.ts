eac788c0a65a60a3faf23e5e71db19dbd7fc7385
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-08-08 15:45:38 +0200

    added circuits.ts

---
+++ src/types/circuit.ts
@@ -0,0 +1,14 @@
+export interface CircuitInfo {
+  id: string;
+  name: string;
+  location: string;
+  country: string;
+  lengthKm: number;
+  laps: number;
+  lapRecordTime: string;
+  lapRecordDriver: string;
+  lapRecordYear: number;
+  turns: number;
+  drsZones: number;
+  description: string;
+}
