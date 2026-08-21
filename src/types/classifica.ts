09a680fe12b840f6113ccbde1938e8dd450e322d
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-08-21 16:54:00 +0200

    Create classifica.ts

---
+++ src/types/classifica.ts
@@ -0,0 +1,13 @@
+export interface PilotaStanding {
+  pilotaId: string;
+  pilotaName: string;
+  teamName: string;
+  points: number;
+}
+
+export interface TeamStanding {
+  teamId: string;
+  teamName: string;
+  colorHex: string;
+  points: number;
+}
