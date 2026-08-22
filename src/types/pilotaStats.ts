a5f6e18ddc96a4615d05dacaaccb1a9c0aff5041
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-08-22 13:38:29 +0200

    Create pilotaStats.ts

---
+++ src/types/pilotaStats.ts
@@ -0,0 +1,13 @@
+export interface PilotaStats {
+  pilotaId: string;
+  pilotaName: string;
+  teamName: string;
+  nationality: string;
+  number: number;
+  points: number;
+  races: number;
+  wins: number;
+  podiums: number;
+  fastestLaps: number;
+  dnfs: number;
+}
